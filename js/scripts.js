85012f98843fa5bef429293e9cfd2ba69578cedc
fenris <Fenris-v>
2021-01-22 23:19:24 +0300

    adaptive

--- js/scripts.js
+++ js/scripts.js
@@ -32,12 +32,28 @@ $(document).ready(() => {
         nextArrow: '<a href="#" class="slick-arrow slick-next"><img src="images/right-arrow.png" alt="arrow"></a>',
         prevArrow: '<a href="#" class="slick-arrow slick-prev"><img src="images/right-arrow.png" alt="arrow"></a>',
         responsive: [{
-            // breakpoint: 640,
-            // settings: {
-            //     slidesToShow: 1
-            // }
-        }]
-    })
+            breakpoint: 1000,
+            settings: {
+                slidesToShow: 3,
+                arrows: false
+            }
+        },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false
+                }
+            }
+        ]
+    });
 
     $('.teachers__slider').slick({
         infinite: true,
@@ -50,10 +66,80 @@ $(document).ready(() => {
         nextArrow: '<a href="#" class="slick-arrow slick-next"><img src="images/right-arrow.png" alt="arrow"></a>',
         prevArrow: '<a href="#" class="slick-arrow slick-prev"><img src="images/right-arrow.png" alt="arrow"></a>',
         responsive: [{
-            // breakpoint: 640,
-            // settings: {
-            //     slidesToShow: 1
-            // }
-        }]
-    })
+            breakpoint: 1000,
+            settings: {
+                slidesToShow: 3,
+                arrows: false
+            }
+        },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false
+                }
+            }
+        ]
+    });
+
+    $('.partners__slider').slick({
+        infinite: true,
+        arrows: true,
+        dots: true,
+        slidesToShow: 5,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 1000,
+        nextArrow: '<a href="#" class="slick-arrow slick-next"><img src="images/right-arrow.png" alt="arrow"></a>',
+        prevArrow: '<a href="#" class="slick-arrow slick-prev"><img src="images/right-arrow.png" alt="arrow"></a>',
+        responsive: [{
+            breakpoint: 1000,
+            settings: {
+                slidesToShow: 3,
+                arrows: false
+            }
+        },
+            {
+                breakpoint: 640,
+                settings: {
+                    slidesToShow: 2,
+                    arrows: false
+                }
+            }
+        ]
+    });
+
+    $('.firstScreen__slider').slick({
+        infinite: true,
+        arrows: false,
+        dots: true,
+        slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 1000
+    });
+
+    /**
+     * FANCYBOX
+     */
+    $('[data-fancybox-modal]').fancybox({
+        trapFocus: true,
+        autoFocus: false,
+        touch: false,
+    });
+
+    /**
+     * HAMBURGER
+     */
+    $('.menu-toggle-inner').on('click', () => {
+        $('.adaptive-menu-toggle').toggleClass('adaptive-menu-toggle--open');
+        $('.headerNav_adaptive').toggleClass('header_opened');
+    });
 });
